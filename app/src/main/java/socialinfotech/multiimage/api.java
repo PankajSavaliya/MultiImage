2a590ca601c21c9744dd4f230000f68c82a31036
Pankaj Savaliya <pankaj.savaliya1111@gmail.com>
2016-08-31 12:09:44 +0530

    Update api.java

--- app/src/main/java/socialinfotech/multiimage/api.java
+++ app/src/main/java/socialinfotech/multiimage/api.java
@@ -9,7 +9,7 @@ import rx.Observable;
  * Created by pankaj on 31/08/16.
  */
 public interface api {
-    String API="http://52.33.130.199/viva/api/v1";
+    String API="http://www.example/api/v1";
 
     @Multipart
     @POST("/upload")
